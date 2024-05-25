ddee2b9750194ae65836bbd3cc25ad70743f1ee5
Dana Ximena Mendoza Medellín <ximena.mendoza04@gmail.com>
2024-05-25 15:38:32 -0600

    Modificación al login para acceder como admin o usuario

--- login.php
+++ login.php
@@ -16,12 +16,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //$hashed_password = md5($password);
 
     $credencial = $conn->real_escape_string($credencial);
+    $password = $conn->real_escape_string($password); // Agregando escape para la contraseña
 
     $sql = "SELECT * FROM Usuario WHERE (Email='$credencial' OR Username='$credencial') AND Contraseña='$password'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        header("Location: Inicio.html");
+        $row = $result->fetch_assoc();
+        if ($row["Rol_ID"] == "2") {
+            header("Location: Inicio.html");
+        } else if ($row["Rol_ID"] == "1") {
+            header("Location: InicioAdmin.html");
+        }
         exit();
     } else {
         echo "<script>alert('Correo electrónico/nombre de usuario o contraseña incorrectos.'); window.location.href = 'login.html';</script>";
