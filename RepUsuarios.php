6191df468a60d75d29ef6a01fa7f152bbd2e4d42
Dana Ximena Mendoza Medellín <ximena.mendoza04@gmail.com>
2024-05-25 21:32:47 -0600

    Modificacion de css usuarios

--- RepUsuarios.php
+++ RepUsuarios.php
@@ -26,7 +26,7 @@
             </ul>
         </div>
     </header>
-    <section id="InfoUsuarios" class="seccion">
+    <section id="InfoUsuarios" class="seccion5">
     <div class="Texto1">
         <br><br><br><br><br>
             <?php
